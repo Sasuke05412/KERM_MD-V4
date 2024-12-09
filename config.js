119f7383d4c119efd7a3ed5caa455668d4dcff3d
Sasuke05412 <derisierceline@gmail.com>
2024-12-09 09:13:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DIQFlJwJ#Zidnp0TFoW2hjCwd00YvfhZ2qb2gpyrKpAjVeHN1DwY",
+SESSION_ID: process.env.SESSION_ID || "XQg0AbZC#Je5i6IsgyjoWlhF9dInk4ymIWcxj3gzdhXHnx2bWQY0",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/osriwc.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR IM ONLINE I'M KERM_MD-V4 WHATSAPP BOT\n\n> 𝖡𝖸 𝖪𝖦𝖳𝖤𝖢𝖧",
+ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR IM Sasuke sispann jwe nan koko ah svp",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
@@ -20,6 +20,6 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "𝐁𝐥𝐚𝐜𝐤 𝐠𝐫𝐞𝐲 𝐥𝐞𝐨𝐧𝐢𝐝𝐚𝐬",
+BOT_NAME: process.env.BOT_NAME || "〘𝙏𝙈𝙋〙𓄂✮⃝𝙎𝘼𝙎𝙐𝙆𝙀𝄟✮͢≛✮⃝🕷️",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
